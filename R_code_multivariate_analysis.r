00a91900c0476f9f521ca1e09fdde6ae469dbcb3
margottura <115725645+margottura@users.noreply.github.com>
2022-11-03 18:01:15 +0100

    Create R_code_multivariate_analysis.r

---
+++ R_code_multivariate_analysis.r
@@ -0,0 +1,33 @@
+# R_code_multivar.r
+
+# install.packages("vegan")
+library(vegan)
+
+# setwd("path/lab")
+
+# setwd for Windows
+setwd("C:/Documenti/Laurea magistrale/Monitoring ecosystems change and functioning")
+
+
+#To upload an R project use the following function:
+load("biomes_multivar.RData")
+ls()
+
+# plot per species matrix
+head(biomes)
+
+multivar <- decorana(biomes)
+multivar
+
+plot(multivar)
+
+# biomes names in the graph:
+attach(biomes_types)
+ordiellipse(multivar, type, col=c("black","red","green","blue"), kind = "ehull", lwd=3)
+ordispider(multivar, type, col=c("black","red","green","blue"), label = T)
+
+pdf("multivar.pdf")
+plot(multivar)
+ordiellipse(multivar, type, col=c("black","red","green","blue"), kind = "ehull", lwd=3)
+ordispider(multivar, type, col=c("black","red","green","blue"), label = T)
+dev.off()666
