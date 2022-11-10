dd46c026f04279ada8561bd0c13b521ef7243909
margottura <115725645+margottura@users.noreply.github.com>
2022-11-10 17:19:20 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -5,5 +5,17 @@ install.packages("raster")
 
 library(raster)
 
+# We set the working directory:
+setwd("C:/Documenti/Laurea magistrale/Monitoring ecosystems change and functioning")
 
+# We are going to upload the data, using a function called "brick()"
 
+brick("p224r63_2011_masked.grd")
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+
+p224r63_2011
+
+plot(p224r63_2011)
+
+cl <- colorRampPalette(c('black','grey','light grey'))(100)  
+plot(p224r63_2011, col=cl)
