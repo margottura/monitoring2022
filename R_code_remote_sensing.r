61d991a53f380e2453b3dba94dee400b651ae638
margottura <115725645+margottura@users.noreply.github.com>
2022-11-11 17:52:45 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -19,3 +19,27 @@ plot(p224r63_2011)
 
 cl <- colorRampPalette(c('black','grey','light grey'))(100)  
 plot(p224r63_2011, col=cl)
+
+# To see only the plot of the first band we do this
+plot(p224r63_2011$B1_sre, col=cl)
+
+# If you know the position of the band you can also do this
+plot(p224r63_2011[[1]], col=cl)
+
+# Excercise: change the color ramp palette with colours drom dark to light blue
+cl <- colorRampPalette(c('dark blue','blue','light blue'))(100) 
+plot(p224r63_2011, col=cl)
+
+# If we want to put images in a row we do this:
+
+par(mfrow=c(1,2))
+plot(p224r63_2011[[1]], col=cl)
+plot(p224r63_2011[[2]], col=cl)
+
+# Excercise: put all the 4 images in 2 different rows
+
+par(mfrow=c(2,2))
+plot(p224r63_2011[[1]], col=cl)
+plot(p224r63_2011[[2]], col=cl)
+plot(p224r63_2011[[3]], col=cl)
+plot(p224r63_2011[[4]], col=cl)
