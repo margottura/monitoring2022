64a22f247e911bbf646b1e449ecb1f340e00ee6c
margottura <115725645+margottura@users.noreply.github.com>
2022-11-25 17:28:24 +0100

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -1,176 +1,132 @@
-# Calculating vegetation indices from remote sensing:
+# Calculating vegetation indices from remote sensing
+# We want to see if trees had been cutted or nope
+
+# Recalling the package we need as usual
 library(raster)
 
-setwd("C:/Documenti/Laurea magistrale/Monitoring ecosystems change and functioning")
+# Setting the folder as usual, and taking the data we need
 
-# Exercise: import the first file -> defor1.png -> give it the name l1992
 l1992 <- brick("defor1.png")
 
+# Our bands are: 1 NearInfraRed, 2 Red, 3 Green
+# Now we plot our image
 plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
 
-# layer 1 = NIR
-# layer 2 = red
-# layer 3 = green
-
-# Exercise: import the second file -> defor2_.jpg -> give it the name l2006
+# And then for the other image
 l2006 <- brick("defor2.png")
-l2006
-
 plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
 
-# Exercise: plot in a multiframe the two images with one on top of the other
+# We plot them together to see the same image in two different periods
 par(mfrow=c(2,1))
 plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
 plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
 
-# DVI Difference Vegetation Index
-dvi1992 = l1992[[1]] - l1992[[2]]
-# or:
-# dvi1992 = l1992$defor1_.1 - l1992$defor1_.2
-dvi1992
+# Now we are going to calculate some indices to demonstrate that there were some big changes in that area
+# We first caluclate de vegetation indices and then we compare the final results
+# Let's make my fancy palette first
+cl <- colorRampPalette(c("darkblue", "yellow", "red", "black"))(100)
 
-cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
+# REMINDER: NIR - R = DVI
+# Now we calculate the index and we plot them
+dvi1992 <- l1992[[1]] - l1992[[2]]
 plot(dvi1992, col=cl)
 
-# DVI Difference Vegetation Index
-dvi2006 = l2006[[1]] - l2006[[2]]
-dvi2006
-
+dvi2006 <- l2006[[1]] - l2006[[2]]
 plot(dvi2006, col=cl)
 
 
-# DVI difference in time
-dvi_dif = dvi1992 - dvi2006
-cld <- colorRampPalette(c('blue','white','red'))(100)
-plot(dvi_dif, col=cld)
+# Let's see now if someone cutted some trees  :(
 
-#- day 2
+# Now we need to racall another package that we use for classification
+library(RStoolbox)
 
-# Range DVI (8 bit): -255 a 255
-# Range NDVI (8 bit): -1 a 1
+# Threshold for trees
+# To do the classifications we use the function "unsuperClass()"
+d1c <- unsuperClass(l1992, nClasses=2)
+# Note: we have to say the number of classes we want
+plot(d1c$map)
 
-# Range DVI (16 bit): -65535 a 65535
-# Range NDVI (16 bit): -1 a 1
+# To see the number of pixels of our two classes we do like this
+freq(d1c$map)
+# Class 1 = forest - 305131
+# Class 2 = human impact - 36161
 
-# Hence, NDVI can be used to compare images with a different radiometric resolution
+# Now we caluclate the proportions of the nature and the human impacts
+# This one for forest
+f1992 <- 305131 / (305131 + 36161)
+# This one for human impacts
+h1992 <- 36161 / (305131 + 36161)
 
-# NDVI 1992
-dvi1992 = l1992[[1]] - l1992[[2]]
-ndvi1992 = dvi1992 / (l1992[[1]] + l1992[[2]])
-# or
-ndvi1992 = (l1992[[1]] - l1992[[2]]) / (l1992[[1]] + l1992[[2]])
+# Let's do the same for 2006
+d2c <- unsuperClass(l2006, nClasses=2)
 
-cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
-plot(ndvi1992, col=cl)
+freq(d2c$map)
+# Class 1 = forest - 178138
+# Class 2 = human impact - 164588
 
-# multiframe con il plotRGB dell'immagine sopra
-# e l'ndvi sotto
+f2006 <- 178138 / (178138 + 164588)
+h2006 <- 164588 / (178138 + 164588)
 
-# Multiframe with plotRGB on top of the NDVI image
-par(mfrow=c(2,1))
-plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
-plot(ndvi1992, col=cl)
-
-# 2006
-ndvi2006 = dvi2006 / (l2006[[1]] + l2006[[2]])
-
-# Multiframe with NDVI1992 on top of the NDVI2006 image
-par(mfrow=c(2,1))
-plot(ndvi1992, col=cl)
-plot(ndvi2006, col=cl)
-
-# Automatic spectral indices by the spectralIndices function
-si1992 <- spectralIndices(l1992, green=3, red=2, nir=1)
-plot(si1992,col=cl)
-
-si2006 <- spectralIndices(l2006, green=3, red=2, nir=1)
-plot(si2006,col=cl)
-
-### rasterdiv
-# plot(copNDVI)
+# The proportion we calculated are these
+# Class 1, 1992 = forest - 0.8940467
+# Class 2, 1992 = human impact - 0.1059533
+# Class 1, 2006 = forest - 0.519768
+# Class 2, 2006 = human impact - 0.480232
 
 
-# R_code_land_cover.r
+# Now we make our database to study the changes
+landcover <- c("Forset", "Humans")
+percent_1992 <- c(89.40, 10.60)
+percent_2006 <- c(51.98, 48.02)
 
-library(raster)
-library(RStoolbox) # classification
-# install.packages("ggplot2")
+# In R the database are called dataframe, we use the function "data.frame()"
+perc <- data.frame(landcover, percent_1992, percent_2006)
+
+# Now we have to use the package "ggplot2" so we have to recall it
 library(ggplot2)
-# install.packages("patchwork")
-library(patchwork) # for grid.arrange plotting
+# And we plot our data with a fancy histogram yey
+ggplot(perc, aes(x=landcover, y=percent_1992, color=landcover)) + geom_bar(stat="identity", fill="chartreuse")
+ggplot(perc, aes(x=landcover, y=percent_2006, color=landcover)) + geom_bar(stat="identity", fill="chartreuse")
 
-setwd("~/lab/") # Linux
-# setwd("C:/lab/") # Windows
-# setwd("/Users/name/Desktop/lab/") # Mac
+# Now we use another fancy package called "patchwork"
+install.packages("patchwork")
+library(patchwork)
 
-# NIR 1, RED 2, GREEN 3
-
-defor1 <- brick("defor1.png")
-plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
-ggRGB(defor1, r=1, g=2, b=3, stretch="lin")
-
-defor2 <- brick("defor2.png")
-plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
-ggRGB(defor2, r=1, g=2, b=3, stretch="lin")
-
-par(mfrow=c(1,2))
-plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
-plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
-
-# multiframe with ggplot2 and gridExtra
-p1 <- ggRGB(defor1, r=1, g=2, b=3, stretch="lin")
-p2 <- ggRGB(defor2, r=1, g=2, b=3, stretch="lin")
+# We assign the two plots to objects and we make one plus the other
+p1 <- ggplot(perc, aes(x=landcover, y=percent_1992, color=landcover)) + geom_bar(stat="identity", fill="chartreuse")
+p2 <- ggplot(perc, aes(x=landcover, y=percent_2006, color=landcover)) + geom_bar(stat="identity", fill="chartreuse")
+# And now we can see the two histograms one beside the other to have a more clear look
 p1 + p2
 
-# unsupervised classification
-d1c <- unsuperClass(defor1, nClasses=2)
-plot(d1c$map)
-# class 1: forest
-# class 2: agriculture
+# Just for fun we try to put the first plot on top of the other
+p1 / p2
 
-# set.seed() would allow you to attain the same results ...
 
-d2c <- unsuperClass(defor2, nClasses=2)
-plot(d2c$map)
-# class 1: agriculture
-# class 2: forest
+# Now we plot the images in RGB
+# Band 1 is the NIR
+plotRGB(l1992, r=1, g=2, b=3)
+# Or we can do like this
+ggRGB(l1992, 1, 2, 3)
 
-d2c3 <- unsuperClass(defor2, nClasses=3)
-plot(d2c3$map)
+# But we can plot also the DVI (Difference Vegetation Index), as above
+dvi1992 = dvi1992 <- l1992[[1]] - l1992[[2]]
+plot(dvi1992, col=cl)
 
-# frequencies
-freq(d1c$map)
-#   value  count
-# [1,]     1 306583
-# [2,]     2  34709
+# We need a new package called "viridis" to make use of a new function for daltonic people to see all the difference in maps
+install.packages("viridis")
+library(viridis)
 
-s1 <- 306583 + 34709
+# Or we can make use of ggplot with a new geometry, geom_raster
+pp1 <- ggplot() + geom_raster(dvi1992, mapping=aes(x=x, y=y, fill=layer)) + scale_fill_viridis(option="viridis")
+# We used also the function "scale_fill_viridis()" to make the map visible for daltonic people, using "viridis" in the options
+# We can use whatever option we want xd
+pp2 <- ggplot() + geom_raster(dvi1992, mapping=aes(x=x, y=y, fill=layer)) + scale_fill_viridis(option="magma")
 
-prop1 <- freq(d1c$map) / s1
-# prop forest: 0.8983012
-# prop agriculture: 0.1016988
+# And we plot the two plots together
+pp1 + pp2
 
-s2 <- 342726
-prop2 <- freq(d2c$map) / s2
-# prop forest: 0.5206958
-# prop agriculture: 0.4793042
 
-# build a dataframe
-cover <- c("Forest","Agriculture")
-percent_1992 <- c(89.83, 10.16)
-percent_2006 <- c(52.06, 47.93)
 
-percentages <- data.frame(cover, percent_1992, percent_2006)
-percentages
-
-# let's plot them!
-ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white")
-ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="identity", fill="white")
-
-p1 <- ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white")
-p2 <- ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="identity", fill="white")
-
-grid.arrange(p1, p2, nrow=1)
 
 
 
