d38909824896c4eaf7faac4aa471d718d97d8927
margottura <115725645+margottura@users.noreply.github.com>
2022-12-02 17:43:48 +0100

    Update R_code_Greenland_ice_melt.r

--- R_code_Greenland_ice_melt.r
+++ R_code_Greenland_ice_melt.r
@@ -24,6 +24,17 @@ p2 <- ggplot() + geom_raster(lst_2000, mapping=aes(x=x, y=y, fill= lst_2000)) + 
 
 p1 + p2
 
+# options:
+# • "magma" (or "A")
+# • "inferno" (or "B")
+# • "plasma" (or "C")
+# • "viridis" (or "D")
+# • "cividis" (or "E")
+# • "rocket" (or "F")
+# • "mako" (or "G")
+# • "turbo" (or "H")
+
+
 lst_2005 <- raster ("lst_2005.tif")
 lst_2010 <- raster ("lst_2010.tif")
 lst_2015 <- raster ("lst_2015.tif")
@@ -49,6 +60,35 @@ TGr
 
 plot(TGr)
 
-p1 <- ggplot() + geom_raster(TGr [[1]], mapping=aes(x=x, y=y, fill= lst_2000)) + scale_fill_viridis(option="magma", alpha=0,2, direction=-1) + ggtitle("Temperature 2000")
+#plot them together via patchwork
+p1 <- ggplot() +
+geom_raster(TGr[[1]], mapping = aes(x=x, y=y, fill=lst_2000)) +
+ggtitle("lst 2000") +
+scale_fill_viridis(option="inferno", alpha=0.8, direction=-1)
 
-p2 <-ggplot() + geom_raster(TGr [[1]], mapping=aes(x=x, y=y, fill= lst_2000)) + scale_fill_viridis(option="magma", alpha=0,2, direction=-1) + ggtitle("Temperature 2000")
+p2 <- ggplot() +
+geom_raster(TGr[[4]], mapping = aes(x=x, y=y, fill=lst_2015)) +
+ggtitle("lst 2015") +
+scale_fill_viridis(option="inferno", alpha=0.8, direction=-1)
+
+p1 + p2
+
+# Excercise: make the different between 2015 and 2000
+2015-2000
+dift = TGr[[4]] - TGr [[1]]
+dift
+
+#Let's plot dift
+
+p3 <- ggplot() +
+geom_raster(dift, mapping = aes(x=x, y=y, fill=layer)) +
+ggtitle("Difference of Temperature from 2000 to 2015") +
+scale_fill_viridis(option="inferno", alpha=0.8, direction=1)
+
+p1+p2+p3
+
+
+plotRGB(TGr, r=1, g=2, b=4, stretch="lin")
+plotRGB(TGr, r=1, g=2, b=4, stretch="hist")
+
+
